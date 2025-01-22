2d601fbf472fb74d133995539ae2c8c04d078412
Thorsten Ball <mrnugget@gmail.com>
2025-01-22 06:18:44 +0100

    Remove tokio/reqwest/async and use ureq instead

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use reqwest;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -16,12 +15,12 @@ struct ClaudeResponse {
 }
 
 #[derive(Debug)]
-struct JudgeResult {
+struct Judgement {
     score: f64,
     message: String,
 }
 
-async fn judge_code(code: &str, assertions: Vec<&str>) -> Result<JudgeResult> {
+fn judge_code(code: &str, assertions: Vec<&str>) -> Result<Judgement> {
     let mut fenced_code = String::from("```");
     fenced_code.push_str(code);
     fenced_code.push_str("```");
@@ -40,13 +39,12 @@ async fn judge_code(code: &str, assertions: Vec<&str>) -> Result<JudgeResult> {
     let model = "claude-3-5-haiku-20241022";
 
     let start = std::time::Instant::now();
-    let client = reqwest::Client::new();
-    let response = client
-        .post("https://api.anthropic.com/v1/messages")
-        .header("x-api-key", api_key)
-        .header("anthropic-version", "2023-06-01")
-        .header("content-type", "application/json")
-        .json(&json!({
+
+    let response: ClaudeResponse = ureq::post("https://api.anthropic.com/v1/messages")
+        .set("x-api-key", &api_key)
+        .set("anthropic-version", "2023-06-01")
+        .set("content-type", "application/json")
+        .send_json(json!({
             "model": model,
             "temperature": 0.0,
             "messages": [{
@@ -54,28 +52,25 @@ async fn judge_code(code: &str, assertions: Vec<&str>) -> Result<JudgeResult> {
                 "content": prompt
             }],
             "max_tokens": 1024
-        }))
-        .send()
-        .await?;
-    let duration = start.elapsed();
+        }))?
+        .into_json()?;
 
-    let result: ClaudeResponse = response.json().await?;
+    let duration = start.elapsed();
     println!("Request took: {:?}", duration);
 
-    let (message, score_text) = result.content[0]
+    let (message, score_text) = response.content[0]
         .text
         .rsplit_once('\n')
         .ok_or(anyhow::anyhow!("Failed to parse score"))?;
     let score = score_text.parse::<f64>()?;
 
-    Ok(JudgeResult {
+    Ok(Judgement {
         score,
         message: message.trim().into(),
     })
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let assertions = vec![
         "[MUST] The year of the copyright notice has to be 2025.",
         "[MUST] The link to the Twitter profile has to be to @thorstenball",
@@ -85,7 +80,7 @@ async fn main() -> Result<()> {
     ];
     let code = include_str!("../data/thorstenball.com.html");
 
-    let result = judge_code(code, assertions).await?;
+    let result = judge_code(code, assertions)?;
     let escape_code_red = "\x1b[31m";
     let escape_code_green = "\x1b[32m";
     let escape_code_reset = "\x1b[0m";
